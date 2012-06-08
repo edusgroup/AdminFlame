9da4ff22c1f5af8f2561ca6108203e524f9840da
Козленко. В.Л <www.dft@mail.ru>
2012-06-08 22:18:51 +0400

    Исправлена бага после переименовывания статей

--- res/plugin/ckeditor/config.js
+++ res/plugin/ckeditor/config.js
@@ -1,127 +1,126 @@
 ﻿/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+ Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function( config )
-{
+CKEDITOR.editorConfig = function (config) {
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
-   // config.uiColor = '#AADC6E';
-    
-    config.enterMode = CKEDITOR.ENTER_BR ;            // p | div | br
-    config.shiftEnterMode = CKEDITOR.ENTER_P ;    // p | div | br
-	
+    // config.uiColor = '#AADC6E';
+
+    config.enterMode = CKEDITOR.ENTER_BR;            // p | div | br
+    config.shiftEnterMode = CKEDITOR.ENTER_P;    // p | div | br
+
     config.toolbar = 'Full';
 
     config.toolbar_Full =
-    [
-    {
-        name: 'document', 
-        items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ]
-    },
-{
-        name: 'clipboard', 
-        items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ]
-    },
-{
-        name: 'editing', 
-        items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ]
-    },
-{
-        name: 'forms', 
-        items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 
-        'HiddenField' ]
-    },
-    '/',
-    {
-        name: 'basicstyles', 
-        items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ]
-    },
-{
-        name: 'paragraph', 
-        items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ]
-    },
-{
-        name: 'links', 
-        items : [ 'Link','Unlink','Anchor' ]
-    },
-{
-        name: 'insert', 
-        items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ]
-    },
-    '/',
-    {
-        name: 'styles', 
-        items : [ 'Styles','Format','Font','FontSize' ]
-    },
-{
-        name: 'colors', 
-        items : [ 'TextColor','BGColor' ]
-    },
-{
-        name: 'tools', 
-        items : [ 'Maximize', 'ShowBlocks','-','About' ]
-    }
-    ];
+        [
+            {
+                name:'document',
+                items:[ 'Source', '-', 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', '-', 'Templates' ]
+            },
+            {
+                name:'clipboard',
+                items:[ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ]
+            },
+            {
+                name:'editing',
+                items:[ 'Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt' ]
+            },
+            {
+                name:'forms',
+                items:[ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
+                    'HiddenField' ]
+            },
+            '/',
+            {
+                name:'basicstyles',
+                items:[ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ]
+            },
+            {
+                name:'paragraph',
+                items:[ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ]
+            },
+            {
+                name:'links',
+                items:[ 'Link', 'Unlink', 'Anchor' ]
+            },
+            {
+                name:'insert',
+                items:[ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ]
+            },
+            '/',
+            {
+                name:'styles',
+                items:[ 'Styles', 'Format', 'Font', 'FontSize' ]
+            },
+            {
+                name:'colors',
+                items:[ 'TextColor', 'BGColor' ]
+            },
+            {
+                name:'tools',
+                items:[ 'Maximize', 'ShowBlocks', '-', 'About' ]
+            }
+        ];
 
     config.toolbar_Article =
-    [
-    {
-        name: 'document', 
-        items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ]
-    },
-{
-        name: 'clipboard', 
-        items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ]
-    },
-{
-        name: 'editing', 
-        items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ]
-    },
-    '/',
-    {
-        name: 'basicstyles', 
-        items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ]
-    },
-{
-        name: 'paragraph', 
-        items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ]
-    },
-{
-        name: 'links', 
-        items : [ 'Link','Unlink','Anchor' ]
-    },
-{
-        name: 'insert', 
-        items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak' ]
-    },
-    '/',
-    {
-        name: 'styles', 
-        items : [ 'Styles','Format','Font','FontSize' ]
-    },
-{
-        name: 'colors', 
-        items : [ 'TextColor','BGColor' ]
-    },
-{
-        name: 'tools', 
-        items : [ 'Maximize', 'ShowBlocks' ]
-    }
-    ];
- 
+        [
+            {
+                name:'document',
+                items:[ 'Source', '-', 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', '-', 'Templates' ]
+            },
+            {
+                name:'clipboard',
+                items:[ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ]
+            },
+            {
+                name:'editing',
+                items:[ 'Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt' ]
+            },
+            '/',
+            {
+                name:'basicstyles',
+                items:[ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ]
+            },
+            {
+                name:'paragraph',
+                items:[ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ]
+            },
+            {
+                name:'links',
+                items:[ 'Link', 'Unlink', 'Anchor' ]
+            },
+            {
+                name:'insert',
+                items:[ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak' ]
+            },
+            '/',
+            {
+                name:'styles',
+                items:[ 'Styles', 'Format', 'Font', 'FontSize' ]
+            },
+            {
+                name:'colors',
+                items:[ 'TextColor', 'BGColor' ]
+            },
+            {
+                name:'tools',
+                items:[ 'Maximize', 'ShowBlocks' ]
+            }
+        ];
+
     config.toolbar_Basic =
-    [
-    ['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink','-','About']
-    ];
+        [
+            ['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink', '-', 'About']
+        ];
 
     /*config.filebrowserBrowseUrl = '/?cmd=fileManager&a1=ckedit&type=file';
-    config.filebrowserImageBrowseUrl = '/?cmd=fileManager&a1=ckedit&type=img';
-    config.filebrowserFlashBrowseUrl = '/?cmd=fileManager&a1=ckedit&type=flash';
-    config.filebrowserUploadUrl = 'res/plugin/kcfinder/upload.php?type=files';
-    config.filebrowserImageUploadUrl = 'res/plugin/kcfinder/upload.php?type=images';
-    config.filebrowserFlashUploadUrl = 'res/plugin/kcfinder/upload.php?type=flash';*/
+     config.filebrowserImageBrowseUrl = '/?cmd=fileManager&a1=ckedit&type=img';
+     config.filebrowserFlashBrowseUrl = '/?cmd=fileManager&a1=ckedit&type=flash';
+     config.filebrowserUploadUrl = 'res/plugin/kcfinder/upload.php?type=files';
+     config.filebrowserImageUploadUrl = 'res/plugin/kcfinder/upload.php?type=images';
+     config.filebrowserFlashUploadUrl = 'res/plugin/kcfinder/upload.php?type=flash';*/
 
 
 };
