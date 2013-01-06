ba802e1740e86fadec8cf3248c84252a15e522c9
Козленко В <www.dft@mail.ru>
2013-01-06 23:06:04 +0400

    Новый функционал

--- res/plugin/classes/utils.js
+++ res/plugin/classes/utils.js
@@ -686,3 +686,16 @@ function objToHtmlVar(pObj, pVarName){
     }
     return result;
 }
+
+/**
+ * Заполнение объектов формы значениями из pData
+ * @param pData {Object} содержит название поля в ключе и значение поля в результате.
+ */
+function unserializeForm(idForm, pData){
+	for( var key in pData){
+		var value = pData[key];
+		$(idForm + ' input[type="checkbox"][name="'+ key +'"][value="'+ value +'"],input[type="radio"][name="'+ key +'"][value="'+ value +'"]').attr('checked', 'checked');
+		$(idForm + ' select[name="'+ key +'"],input[type="text"][name="'+ key +'"],input[type="password"][name="'+ key +'"],input[type="hidden"][name="'+ key +'"],textarea[name="'+ key +'"]').val(value);
+	}
+	// func. unserializeForm
+}
